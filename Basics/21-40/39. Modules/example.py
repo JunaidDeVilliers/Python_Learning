d1686fd7a16abcac20973e280f67f64885ac2a48
Junaid <junaiddevilliers@gmail.com>
2025-02-08 21:51:21 +0200

    daily lessons

---
+++ Basics/21-40/39. Modules/example.py
@@ -0,0 +1,5 @@
+def uppercase(name):
+    print(name.upper())
+
+def lowercase(name):
+    print(name.lower())
---
+++ Basics/21-40/39. Modules/modules.py
@@ -0,0 +1,8 @@
+# module - a file containing code you want to include in your program
+#          use 'import' to include a module in your program (built-in or your own)
+#          useful to break up a large program into reusable separate files
+
+import example # importing my self created module
+
+example.lowercase("JUNAID")
+example.uppercase("junaid")
