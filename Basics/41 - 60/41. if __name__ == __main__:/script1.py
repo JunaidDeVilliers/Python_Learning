4fa8ed941a62a432f62b30c68a6c68bc3bf39f1d
Junaid <junaiddevilliers@gmail.com>
2025-02-10 09:02:43 +0200

    daily lessons

---
+++ Basics/41 - 60/41. if __name__ == __main__:/script1.py
@@ -0,0 +1,14 @@
+# if __name__ == '__main__': (this script can be imported or run standalone)
+#                            Functions and classes in this module can be reused without the main block of code executing.
+
+def favorite_food(food):
+    print(f"Your favorite food is {food}")
+
+def main():
+    print("This is script1")
+    favorite_food("pizza")
+    print("Goodbye")
+
+  
+if __name__ == '__main__':
+    main()
