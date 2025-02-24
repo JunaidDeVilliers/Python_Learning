7d18f14463166853e2da2cf5c637e88687628a45
Junaid <junaiddevilliers@gmail.com>
2025-02-24 08:14:56 +0200

    hackerrank challenges

--- Hackerrank challenges/Challenge 2.py
+++ Hackerrank challenges/Challenge 2.py
@@ -1,4 +1,8 @@
-#!/bin/python3
+# Task
+# Given an integer, , perform the following conditional actions:    If is odd, print Weird
+#                                                                   If is even and in the inclusive range of  to , print Not Weird
+#                                                                   If is even and in the inclusive range of  to , print Weird
+#                                                                   If is even and greater than , print Not Weird
 
 import math
 import os
