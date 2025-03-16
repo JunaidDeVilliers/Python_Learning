13757bbca00660abfbad981a29ac9eac7d5015f8
Junaid <junaiddevilliers@gmail.com>
2025-03-16 18:09:19 +0200

    removal

--- Hackerrank challenges/Challenge 5.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    n = 3
-
-    for i in range(0,n):
-        print(i**2)
--- Hackerrank challenges/Challenge 7.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    
-    if n >= 1 and n <= 150:
-        for x in range(1, n+1):
-            print(x, end="")
