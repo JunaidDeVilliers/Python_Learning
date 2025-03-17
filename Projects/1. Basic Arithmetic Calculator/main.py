18883e44a3200933c9e6d04ad1b68f21ea70d774
Junaid <junaiddevilliers@gmail.com>
2025-03-17 10:41:46 +0200

    Calculator program

--- Projects/1. Basic Arithmetic Calculator/main.py
+++ Projects/1. Basic Arithmetic Calculator/main.py
@@ -1,3 +1,5 @@
+from Calculator import Calculate
+
 if __name__ == '__main__':
     print("Welcome to the Basic Arithmetic Calculator")
     
@@ -6,34 +8,27 @@ if __name__ == '__main__':
     num1 = float(input("Enter your first number: "))
     num2 = float(input("Enter your second number: "))
 
-    def add(num1, num2, operator):
-        print(f"{num1} {operator} {num2} = {num1 + num2}")
-
-    def subtract(num1, num2, operator):
-        print(f"{num1} {operator} {num2} = {num1 - num2}")
-
-    def divide(num1, num2, operator):
-        print(f"{num1} {operator} {num2} = {num1 / num2}")
-
-    def multiply(num1, num2, operator):
-        print(f"{num1} {operator} {num2} = {num1 * num2}")
+    calc = Calculate()
 
     while valid_operation == False:
         operator = input("Please enter the operation you would like to perform: ")
-
         match operator:
+
             case "+":
-                add(num1, num2, operator)
+                print(calc.add(num1, num2))
                 valid_operation = True
             case "-":
-                subtract(num1, num2, operator)
+                print(calc.subtract(num1, num2))
                 valid_operation = True
             case "*":
-                multiply(num1, num2, operator)
+                print(calc.multiply(num1, num2))
                 valid_operation = True
             case "/":
-                divide(num1, num2, operator)
+                print(calc.divide(num1, num2))
                 valid_operation = True
             case _:
                 print("Not a valid operation")
 
+# 1. Fix the divide by 0
+# 2. Add process to redo until opting out
+# 3. Add process to ensure a number is input
